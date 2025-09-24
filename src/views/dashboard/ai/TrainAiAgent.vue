4eeb84f4d9c1badabd142dcfe51003e5d205b77b
Hasibul Mahi <hasibulmahi@gmail.com>
2025-09-24 16:31:24 +0600

    feat: faq with all files link added

--- src/views/dashboard/ai/TrainAiAgent.vue
+++ src/views/dashboard/ai/TrainAiAgent.vue
@@ -1,67 +1,61 @@
 <template>
-	<div class="container">
-		<!-- Start Row -->
-		<div class="row">
-			<div class="col-12">
-				<!-- Breadcrumb Start -->
-				<Breadcrumb :items="BreadcrumbItems" />
-				<!-- Breadcrumb End -->
-			</div>
-		</div>
-		<!-- End Row -->
-		<div class="row gy-4">
-			<div class="col-lg-12">
-				<!-- Section Title Start -->
-				<SectionTitle :items="SectionTitleData[0]" />
-			</div>
-			<div class="col-12">
-				<div class="row g-3">
-					<div class="col-lg-2">
-						<div class="agent__sidebar">
-							<ul class="nav flex-column sidebar__menu">
-								<li>
-									<router-link :to="{ name: 'train-ai-agent' }">
-										Dashboard
-									</router-link>
-								</li>
-								<li>
-									<router-link :to="{ name: 'train-ai-agent-pdf' }">
-										PDF
-									</router-link>
-								</li>
-								<li>
-									<router-link :to="{ name: 'train-ai-agent-text-file' }">Text File</router-link>
-								</li>
-								<li>
-									<router-link to="/faq">FAQ</router-link>
-								</li>
-								<li>
-									<router-link to="/knowledge-base">Knowledge Base</router-link>
-								</li>
-								<li>
-									<router-link to="/product">Product</router-link>
-								</li>
-								<li>
-									<router-link to="/service">Service</router-link>
-								</li>
-								<li>
-									<router-link to="/company-info">Company Info</router-link>
-								</li>
-								<li>
-									<router-link to="/open-ai-configure">
-										Open AI Configure
-									</router-link>
-								</li>
-							</ul>
-						</div>
-					</div>
-					<div class="col-lg-10">
-						<router-view></router-view>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="container">
+    <!-- Start Row -->
+    <div class="row">
+      <div class="col-12">
+        <!-- Breadcrumb Start -->
+        <Breadcrumb :items="BreadcrumbItems" />
+        <!-- Breadcrumb End -->
+      </div>
+    </div>
+    <!-- End Row -->
+    <div class="row gy-4">
+      <div class="col-lg-12">
+        <!-- Section Title Start -->
+        <SectionTitle :items="SectionTitleData[0]" />
+      </div>
+      <div class="col-12">
+        <div class="row g-3">
+          <div class="col-lg-2">
+            <div class="agent__sidebar">
+              <ul class="nav flex-column sidebar__menu">
+                <li>
+                  <router-link :to="{ name: 'train-ai-agent' }"> Dashboard </router-link>
+                </li>
+                <li>
+                  <router-link :to="{ name: 'train-ai-agent-pdf' }"> PDF </router-link>
+                </li>
+                <li>
+                  <router-link :to="{ name: 'train-ai-agent-text-file' }">Text File</router-link>
+                </li>
+                <li>
+                  <router-link :to="{ name: 'train-ai-agent-faq' }"> FAQ </router-link>
+                </li>
+                <li>
+                  <router-link to="/knowledge-base">Knowledge Base</router-link>
+                </li>
+                <li>
+                  <router-link to="/product">Product</router-link>
+                </li>
+                <li>
+                  <router-link to="/service">Service</router-link>
+                </li>
+                <li>
+                  <router-link to="/company-info">Company Info</router-link>
+                </li>
+                <li>
+                  <router-link to="/open-ai-configure"> Open AI Configure </router-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-10">
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -89,5 +83,4 @@ const SectionTitleData = ref([
     badge: 'Last Train: 1 days ago',
   },
 ])
-
 </script>
---
+++ src/views/dashboard/ai/component/Faq.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="card px-20 py-20 radius-20">
+    <div class="card__header mb-3">
+      <h4 class="title">All FAQ</h4>
+      <div class="btn__group">
+        <router-link :to="{ name: 'train-ai-agent-add-faq' }" class="sg-btn sg-btn-dark"
+          >Add FAQ</router-link
+        >
+      </div>
+    </div>
+    <div class="card__body">
+      <!-- File Item Start -->
+      <div class="file__item" v-for="items in paginatedFaqData" :key="items.id">
+        <div class="">
+          <h5 class="text-black fw-medium text-14">{{ items.id }}. {{ items.question }}</h5>
+          <div class="text-14 text-content">
+            <span class="text-black mr-05">Answer:</span>{{ items.answer }}
+          </div>
+        </div>
+
+        <div class="action__btn">
+          <router-link :to="{ name: 'train-ai-agent-view-faq' }" class="button">
+            <HugeiconsIcon :icon="ViewIcon" />
+          </router-link>
+          <router-link :to="{ name: 'train-ai-agent-edit-faq' }" class="button">
+            <HugeiconsIcon :icon="Edit03Icon" />
+          </router-link>
+          <button type="button" class="button delete">
+            <HugeiconsIcon :icon="Delete02Icon" />
+          </button>
+        </div>
+      </div>
+      <!-- Pagination Start  -->
+      <Pagination :totalItems="FaqData.length" :perPage="perPage" v-model="currentPage" />
+      <!-- Pagination End  -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import Pagination from '@/components/Pagination.vue'
+import { Edit03Icon, Delete02Icon, ViewIcon } from '@hugeicons/core-free-icons'
+import { HugeiconsIcon } from '@hugeicons/vue'
+
+// FaqData
+
+const FaqData = ref([
+  {
+    id: '1',
+    question: 'Question: What is SpaGreen Creative?',
+    answer: 'SpaGreen Creative is a top software company in Bangladesh ...',
+  },
+  {
+    id: '2',
+    question: 'Question: How can you growth your digital business?',
+    answer: 'SpaGreen Creative is a top software company in Bangladesh ...',
+  },
+  {
+    id: '3',
+    question: 'Question: Which solution provide SpaGreen Creative?',
+    answer: 'SpaGreen Creative is a top software company in Bangladesh ...',
+  },
+  {
+    id: '4',
+    question: 'Question: What is delivery time after project engaged?',
+    answer: 'SpaGreen Creative is a top software company in Bangladesh ...',
+  },
+  {
+    id: '5',
+    question: 'Question: How expert Spagreen Creative team? ',
+    answer: 'SpaGreen Creative is a top software company in Bangladesh ...',
+  },
+  {
+    id: '6',
+    question: 'Question: How expert Spagreen Creative team? ',
+    answer: 'SpaGreen Creative is a top software company in Bangladesh ...',
+  },
+])
+
+// Pagination Logic
+const perPage = ref(5)
+const currentPage = ref(1)
+
+const paginatedFaqData = computed(() => {
+  const start = (currentPage.value - 1) * perPage.value
+  const end = start + perPage.value
+  return FaqData.value.slice(start, end)
+})
+</script>
